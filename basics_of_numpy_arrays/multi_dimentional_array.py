832be5ae43e8dbb94e4dc85f3cd39801fff993fc
rhythmplayz <114134549+rhythmplayz@users.noreply.github.com>
2026-01-12 21:54:30 +0600

    numpy array types

---
+++ basics_of_numpy_arrays/multi_dimentional_array.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a2D = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+arr2D = np.array(a2D)
+
+print("Python multidimensional list (different data types are supported): ",a2D)
+print("numpy multidimensional array (different data types are not supported): ",arr2D)
---
+++ basics_of_numpy_arrays/one_dimentional_array.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = [1,2,3,4,5]
+
+arr = np.array(a)
+
+print("Python List: ",a)
+print("Numpy Array: ",arr)
+
+print(type(a))
+print(type(arr))
